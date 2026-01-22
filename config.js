114379cec02783fcb0a68152765ba32c7db38d4e
ɖʊʀɛʐ☆ <lorddurez@gmail.com>
2026-01-22 17:57:14 +0100

    Add configuration for bot settings

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  botName: "ɖʊʀɛʐ☆X1",
+  ownerName: "ɖʊʀɛʐ☆",
+  ownerNumber: ["2348062641931"],
+  prefix: ".",
+  support: "https://whatsapp.com/channel/0029VbCEE9mFHWq4rjRLRp3n"
+};
